46a935a0206efdab7061c541060e20ce850a189f
estwind <estwindexodars@gmail.com>
2022-04-29 20:37:33 +0700

    use api test

--- src/components/pages/search/Search.jsx
+++ src/components/pages/search/Search.jsx
@@ -1,14 +1,32 @@
 import "./search.scss";
 import SearchPanel from "./searchPanel/SearchPanel";
 import SearchItem from "./../../templates/searchItem/SearchItem";
+import { useEffect } from "react";
 const Search = () => {
+  const postData = async () => {
+    const response = await fetch(
+      "http://api.duma.gov.ru/api/b7886da6f2500ed765c67bd8e9904b3eb83b1c23/topics.json"
+    );
+    return await response.json();
+  };
+
+  useEffect(() => {
+    postData().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <section className="search">
-      <SearchPanel/>
+      <SearchPanel />
       <div className="searchResults">
-        <SearchItem/>
+        <SearchItem />
       </div>
     </section>
   );
 };
 export default Search;
+// method: 'POST',
+// headers: {
+//   'Origin': 'myregulationgov.com'
+// },
