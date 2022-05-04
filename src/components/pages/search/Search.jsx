f6c8e7cb35bf1bedf20077e9c2938e6baaa95b7c
estwind <estwindexodars@gmail.com>
2022-05-04 21:46:36 +0700

    fix

--- src/components/pages/search/Search.jsx
+++ src/components/pages/search/Search.jsx
@@ -2,6 +2,7 @@ import "./search.scss";
 import SearchPanel from "./searchPanel/SearchPanel";
 import SearchItem from "./../../templates/searchItem/SearchItem";
 import { useEffect, useState } from "react";
+import fetchJsonp from 'fetch-jsonp';
 const Search = () => {
   // const postData = async () => {
   //   const response = await fetch(
@@ -15,45 +16,21 @@ const Search = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "http://api.duma.gov.ru/api/0fe8301c7a1246606877db2f9835bb9c4d9b24a5/topics.json?app_token=appe6f512ef4104f66ef4aa2f8d7e2fcec46b2fc7c3",
-      {
-        mode: "no-cors",
-      }
-    ).then(res => res.json())
-    .then(
-      (result) => {
-        setIsLoaded(true);
-        setItems(result);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    )
-    
+    fetchJsonp('http://api.duma.gov.ru/api/0fe8301c7a1246606877db2f9835bb9c4d9b24a5/topics.json?app_token=appe6f512ef4104f66ef4aa2f8d7e2fcec46b2fc7c3')
+      .then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        setItems(json)
+      }).catch(function(ex) {
+        console.log('parsing failed', ex)
+      })
   }, []);
-  // useEffect(() => {
-  //   fetch(
-  //     "http://api.duma.gov.ru/api/0fe8301c7a1246606877db2f9835bb9c4d9b24a5/topics.json?app_token=appe6f512ef4104f66ef4aa2f8d7e2fcec46b2fc7c3", {mode: "no-cors"}
-  //   ).then(response => {
-  //     if (response.ok) {
-  //       return response.json();
-  //     } throw response;
-  //   })
-  //   .then(
-  //     (result) => {
-  //       setIsLoaded(true);
-  //       setItems(result);
-  //     },
-  //     (error) => {
-  //       setIsLoaded(true);
-  //       setError(error);
-  //     }
-  //   )
-    
-  // }, []);
-  console.log(items)
+  useEffect(() => {
+    console.log(items)
+  }, [items]);
+
+  
+  
 
   return (
     <section className="search">
@@ -65,7 +42,3 @@ const Search = () => {
   );
 };
 export default Search;
-// method: 'POST',
-// headers: {
-//   'Origin': 'myregulationgov.com'
-// },
